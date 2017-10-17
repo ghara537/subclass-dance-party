562c829b4d44e974775fbfb85b91af9fd13eb75c
ghara537 <ghara537@gmail.com>
2017-10-17 11:25:20 -0700

    Add SlideDancer and animate left/right sliding

--- src/dancer.js
+++ src/dancer.js
@@ -4,10 +4,12 @@ var Dancer = function(top, left, timeBetweenSteps) {
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   this.$node.append('<img src="images/mj.jpeg">');
+  this.position = {top: top, left: left};
   this.step(timeBetweenSteps);
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
+  
 };
 
 Dancer.prototype.colorPicker = function() {
--- src/slideDancer.js
+++ src/slideDancer.js
@@ -10,10 +10,9 @@ SlideDancer.prototype.constructor = SlideDancer;
 SlideDancer.prototype.step = function(timer) {
   // call the old version of step at the beginning of any call to this new version of step
   Dancer.prototype.step.call(this, timer);
-  this.$node.animate({left: '+50%'});
-  this.$node.animate({left: '-50%'});
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
-  // this.$node.toggle();
+  var oldleft = this.position.left;
+  var newleft = oldleft + 100;
+  this.$node.show({left: oldleft});
+  this.$node.animate({left: newleft});
+  this.$node.animate({left: oldleft});
 };
