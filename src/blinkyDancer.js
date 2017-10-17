096626ed02a0efccb9988beb58683a2d17511dc6
ghara537 <ghara537@gmail.com>
2017-10-16 17:08:43 -0700

    Add FadingDancer and random color generator

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,26 +1,10 @@
-// var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-//   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
-//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-//   // so we must keep a copy of the old version of this function
-
-//   var oldStep = blinkyDancer.step;
-
-//   blinkyDancer.step = function() {
-//     // call the old version of step at the beginning of any call to this new version of step
-//     oldStep();
-//     // toggle() is a jQuery method to show/hide the <span> tag.
-//     // See http://api.jquery.com/category/effects/ for this and
-//     // other effects you can use on a jQuery-wrapped html tag.
-//     blinkyDancer.$node.toggle();
-//   };
-
-//   return blinkyDancer;
-// };
 
 var BlinkyDancer = function(top, left, timeBetweenSteps) {
-  // this = Object.create(BlinkyDancer.prototype);
   Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.css(this.colorPicker());
+  this.$node.addClass('blinky');
+  
   var blinker = this;
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
@@ -34,8 +18,6 @@ var BlinkyDancer = function(top, left, timeBetweenSteps) {
     // other effects you can use on a jQuery-wrapped html tag.
     blinker.$node.toggle();
   };
-  // this.step();
-  // return this
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
